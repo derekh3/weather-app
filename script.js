30c7b470ef6e08316f4ea5f3291adcac46794040
derekleehuang <derekh2008@gmail.com>
2023-01-30 12:43:11 -0700

    Add styling

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-let weatherData = null;
+let weatherTodayData = null;
+let weather5DayData = null;
 async function openPromise(location) {
   try {
     dataPromise = await fetch(
@@ -9,8 +10,28 @@ async function openPromise(location) {
     );
 
     const data = await dataPromise.json();
-    weatherData = data;
+    weatherTodayData = data;
     return data;
+  } catch (error) {
+    console.log(error);
+    if (error.cod === "404") {
+      return error;
+    }
+  }
+}
+
+async function open5DayPromise(lat, lon) {
+  try {
+    dataPromise2 = await fetch(
+      `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=313a3a7aec6418d32f93d9f3924adc85`,
+      {
+        mode: "cors",
+      }
+    );
+
+    const data2 = await dataPromise2.json();
+    weather5DayData = data2;
+    return data2;
   } catch (error) {
     console.log(error);
     if (error.cod === "404") {
@@ -44,17 +65,31 @@ const DOMController = ((doc) => {
     textSubcontainers.forEach((subcontainer) => {
       subcontainer.innerHTML = "";
     });
+    let forecastContainer = doc.querySelector(".five-day-forecast-container");
+    forecastContainer.innerHTML = "";
     loadPage(searchField.value);
+
+    let errorMessage = doc.querySelector(".error-message");
+    errorMessage.remove();
+  };
+
+  // program to convert first letter of a string to uppercase
+  const capitalizeFirstLetter = (str) => {
+    // converting first letter to uppercase
+    const capitalized = str.charAt(0).toUpperCase() + str.slice(1);
+
+    return capitalized;
   };
 
   const errorLocationNotFound = () => {
     let weatherContainer = doc.querySelector(".weather-container");
     let errorMessage = doc.createElement("div");
+    errorMessage.classList.add("error-message");
     errorMessage.textContent = "Location not found";
     weatherContainer.appendChild(errorMessage);
   };
 
-  const addName = (container) => {
+  const addName = (container, weatherData = weatherTodayData) => {
     let nameText = doc.createElement("div");
     let nameDataObject = unpackData.unpackStringArg(weatherData, ["name"]);
     let name = nameDataObject["name"];
@@ -63,7 +98,23 @@ const DOMController = ((doc) => {
     container.appendChild(nameText);
   };
 
-  const addIcon = (container) => {
+  const addDateTime = (container, weatherData = weatherTodayData) => {
+    let dateTimeText = doc.createElement("div");
+    let dateTime = weatherData.dt_txt;
+
+    let dateObject = new Date(Date.parse(dateTime));
+    let dayOfWeek = dateTimeModule.days[dateObject.getDay()];
+    let dayOfMonth = dateObject.getDate();
+    let month = dateTimeModule.months[dateObject.getMonth()];
+    let hours = dateObject.getHours();
+    let minutes = dateObject.getMinutes();
+
+    dateTimeText.textContent = `${dayOfWeek}`;
+    // dateTimeText.textContent = `${dayOfWeek}, ${month} ${dayOfMonth}`;
+    container.appendChild(dateTimeText);
+  };
+
+  const addIcon = (container, weatherData = weatherTodayData) => {
     let icon = doc.createElement("img");
     let iconID = weatherData.weather[0].icon;
     let iconurl = "http://openweathermap.org/img/w/" + iconID + ".png";
@@ -72,15 +123,19 @@ const DOMController = ((doc) => {
     container.appendChild(icon);
   };
 
-  const addDescription = (container) => {
+  const addDescription = (container, weatherData = weatherTodayData) => {
     let descriptionText = doc.createElement("div");
-    let descriptionDataObject = weatherData.weather[0].description;
-    let description = descriptionDataObject;
-    descriptionText.textContent = `Description: ${description}`;
+    let description = weatherData.weather[0].description;
+    let descriptionCapitalized = capitalizeFirstLetter(description);
+    descriptionText.textContent = `${descriptionCapitalized}`;
     container.appendChild(descriptionText);
   };
 
-  const addTemperature = (container) => {
+  const addTemperature = (
+    container,
+    decimalPlaces = 2,
+    weatherData = weatherTodayData
+  ) => {
     let temperatureText = doc.createElement("div");
     let temperatureDataObject = unpackData.unpackStringArg(weatherData, [
       "main",
@@ -89,13 +144,13 @@ const DOMController = ((doc) => {
     let temperatureFahrenheit =
       (temperatureDataObject["temp"] - 273.15) * (9.0 / 5.0) + 32;
 
-    temperatureText.textContent = `Temperature: ${temperatureFahrenheit.toFixed(
-      2
-    )} \u00B0 F`;
+    temperatureText.textContent = `${temperatureFahrenheit.toFixed(
+      decimalPlaces
+    )} \u00B0F`;
     container.appendChild(temperatureText);
   };
 
-  const addHumidity = (container) => {
+  const addHumidity = (container, weatherData = weatherTodayData) => {
     let humidityText = doc.createElement("div");
     let humidityDataObject = unpackData.unpackStringArg(weatherData, [
       "main",
@@ -107,7 +162,7 @@ const DOMController = ((doc) => {
     container.appendChild(humidityText);
   };
 
-  const addCloudiness = (container) => {
+  const addCloudiness = (container, weatherData = weatherTodayData) => {
     let cloudinessText = doc.createElement("div");
     let cloudinessDataObject = unpackData.unpackStringArg(weatherData, [
       "clouds",
@@ -119,38 +174,137 @@ const DOMController = ((doc) => {
     container.appendChild(cloudinessText);
   };
 
+  const initialize5DayForecast = (container) => {
+    for (let i = 0; i < 5; i++) {
+      let newSubcontainer = doc.createElement("div");
+      newSubcontainer.classList.add(`five-day-subcontainer`);
+      newSubcontainer.classList.add(`day-${i}`);
+
+      let dateTimeField = doc.createElement("div");
+      dateTimeField.classList.add(`five-day-dateTime-${i}`);
+      dateTimeField.classList.add(`five-day-dateTime-field`);
+      newSubcontainer.appendChild(dateTimeField);
+
+      let descriptionField = doc.createElement("div");
+      descriptionField.classList.add(`five-day-description-${i}`);
+      descriptionField.classList.add(`five-day-description-field`);
+
+      newSubcontainer.appendChild(descriptionField);
+
+      let temperatureField = doc.createElement("div");
+      temperatureField.classList.add(`five-day-temperature-${i}`);
+      temperatureField.classList.add(`five-day-temperature-field`);
+
+      newSubcontainer.appendChild(temperatureField);
+
+      let iconField = doc.createElement("div");
+      iconField.classList.add(`five-day-icon-${i}`);
+      iconField.classList.add(`five-day-icon-field`);
+      newSubcontainer.appendChild(iconField);
+
+      container.appendChild(newSubcontainer);
+    }
+  };
+
   return {
     addName,
+    addDateTime,
     addIcon,
     addDescription,
     addTemperature,
     addHumidity,
     addCloudiness,
     errorLocationNotFound,
+    initialize5DayForecast,
   };
 })(document);
+
+const dateTimeModule = (() => {
+  const months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+
+  const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return { months, days };
+})();
 
 // openPromise().then((result) => {
 //   weatherData = result;
 // });
 async function loadPage(location = "London") {
   let promise = await openPromise(location);
+
   console.log("HERE");
   console.log(promise.cod);
-  console.log(weatherData);
-  if (promise.cod !== "404") {
+  if (promise.cod === 200) {
+    //call the 5 day weather API
+    let latitude = weatherTodayData.coord.lat;
+    let longitude = weatherTodayData.coord.lon;
+    let promise5Day = await open5DayPromise(latitude, longitude);
+    console.log(weather5DayData);
+
+    //load the data for today's weather
     let nameContainer = document.querySelector(".name-text");
     let iconContainer = document.querySelector(".icon-subcontainer");
     let descriptionContainer = document.querySelector(".description-text");
     let temperatureContainer = document.querySelector(".temperature-text");
     let humidityContainer = document.querySelector(".humidity-text");
     let cloudinessContainer = document.querySelector(".cloudiness-text");
-    DOMController.addName(nameContainer);
+    DOMController.addName(nameContainer, weatherTodayData);
     DOMController.addIcon(iconContainer);
     DOMController.addDescription(descriptionContainer);
     DOMController.addTemperature(temperatureContainer);
     DOMController.addHumidity(humidityContainer);
     DOMController.addCloudiness(cloudinessContainer);
+
+    //load the data for 5 day weather forecast
+    let forecastContainer = document.querySelector(
+      ".five-day-forecast-container"
+    );
+
+    let weather5DayDataArray = [];
+    for (let i = 0; i < 5; i++) {
+      weather5DayDataArray.push(weather5DayData.list[(i + 1) * 8 - 1]);
+    }
+    console.log(weather5DayDataArray);
+    DOMController.initialize5DayForecast(forecastContainer);
+
+    for (let i = 0; i < 5; i++) {
+      let dateTimeContainer = document.querySelector(`.five-day-dateTime-${i}`);
+      DOMController.addDateTime(dateTimeContainer, weather5DayDataArray[i]);
+
+      let iconContainer = document.querySelector(`.five-day-icon-${i}`);
+      DOMController.addIcon(iconContainer, weather5DayDataArray[i]);
+
+      let descriptionContainer = document.querySelector(
+        `.five-day-description-${i}`
+      );
+      DOMController.addDescription(
+        descriptionContainer,
+        weather5DayDataArray[i]
+      );
+
+      let temperatureContainer = document.querySelector(
+        `.five-day-temperature-${i}`
+      );
+      DOMController.addTemperature(
+        temperatureContainer,
+        0,
+        weather5DayDataArray[i]
+      );
+    }
   } else {
     DOMController.errorLocationNotFound();
   }
